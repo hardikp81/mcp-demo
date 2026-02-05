f5e0118e17e2e81848cc17ddda236cfc006666f9
Hardik <hardikpatel_mca@hotmail.com>
2026-02-04 21:24:01 -0800

    Adding logic to connect the MCP server from console application and list the available tools.

--- aspnet_core_v1/aspnet_core_v1/MCP.Demo.ConsoleClient/Program.cs
+++ aspnet_core_v1/aspnet_core_v1/MCP.Demo.ConsoleClient/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ModelContextProtocol.Client;
+
+Console.WriteLine("Waiting for MCP server to start. Press enter once the MCP server is up and running.");
+
+Console.ReadLine();
+
+var transportClient = new HttpClientTransport(new HttpClientTransportOptions()
+{
+    Endpoint = new Uri("http://localhost:5231/"),
+    
+});
+
+Console.WriteLine("Connecting to the MCP server");
+var mcpClient = await McpClient.CreateAsync(transportClient);
+
+Console.WriteLine("Listing available tools from MCP server");
+var mcpTools = await mcpClient.ListToolsAsync();
+foreach(var m in mcpTools)
+{
+    Console.WriteLine(m.Name);
+}
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Processign done");
+
+Console.ReadLine();
